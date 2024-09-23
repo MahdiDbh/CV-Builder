f8c32ce3bfd04b499353c950330d579537f71211
Alviinee <mahdi.debbah@outlook.com>
2024-09-24 00:43:30 +0200

    adding views + fix routes + fix frontend controller

--- app/Http/Controllers/frontend/frontendcontroller.php
+++ app/Http/Controllers/frontend/frontendcontroller.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class frontendcontroller extends Controller
 {
-    //
+    public function index()
+    {
+
+        return view('index');
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,9 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
    <!-- site metas -->
-   <title>diigo</title>gg
+   <title>diigo</title>
    <meta name="keywords" content="">
-   <meta name="description" content="">g
+   <meta name="description" content="">
    <meta name="author" content="">
    <!-- bootstrap css -->
    <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
