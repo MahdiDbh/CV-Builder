44172487fa6f9d81b61dcbf5fea57189d98293f7
Alviinee <mahdi.debbah@outlook.com>
2024-09-27 20:23:31 +0200

    adding Header + footer Blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,71 +41,7 @@
    </div>
    <!-- end loader -->
    <!-- header -->
-   <header>
-      <!-- header inner -->
-      <div class="head_top">
-         <div class="header">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                     <div class="full">
-                        <div class="center-desk">
-                           <div class="logo">
-                              <a href="index.html"><img src="{{asset('fontend/images/logo.png')}}" alt="#" /></a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                           data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false"
-                           aria-label="Toggle navigation">
-                           <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarsExample04">
-                           <ul class="navbar-nav mr-auto">
-                              <li class="nav-item">
-                                 <a class="nav-link" href="#"> Home </a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="#">About</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="#">Contact</a>
-                              </li>
-                           </ul>
-                        </div>
-                     </nav>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <!-- end header inner -->
-         <!-- end header -->
-         <!-- banner -->
-         <section class="banner_main">
-            <div class="container-fluid">
-               <div class="row d_flex">
-                  <div class="col-md-6">
-                     <div class="text-bg">
-                        <h1>Create your Cv</h1>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a
-                           page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
-                           normal distribution of letters,</p>
-                        <a href="#">Start Now</a>
-                     </div>
-                  </div>
-                  <div class="col-md-6">
-                     <div class="text-img">
-                        <figure><img src="{{asset('frontend/images/side.png')}}" alt="#" /></figure>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </section>
-      </div>
-   </header>
+   @include('include.header')
    <!-- end banner -->
    <!-- business -->
 
@@ -156,37 +92,7 @@
    </div>
    <!-- end contact -->
    <!--  footer -->
-   <footer>
-      <div class="footer">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12 ">
-                  <div class="cont">
-                     <h3> <strong class="multi"> Free Multipurpose Responsive</strong><br>
-                        Landing Page 2024
-                     </h3>
-                  </div>
-               </div>
-               <div class="col-md-12">
-                  <ul class="social_icon">
-                     <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                     <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></i></a></li>
-                     <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></i></a></li>
-                  </ul>
-               </div>
-            </div>
-         </div>
-         <div class="copyright">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-12">
-                     <p>Copyright 2019 All Right Reserved By <a href="https://html.design/"> Free html Templates</a></p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-   </footer>
+   @include('include.header')
    <!-- end footer -->
    <!-- Javascript files-->
    <script src="{{asset('frontend/js/jquery.min.js')}}"></script>
